1d3d49dc6c043b0a301f741990ae4daa7f2e4bdc
Juan Muñoz <alexx44@users.noreply.github.com>
2016-08-12 15:55:02 -0400

    Test SHELL

--- gruntfile.js
+++ gruntfile.js
@@ -15,12 +15,23 @@ module.exports = function(grunt){
                 src: 'unidos.js',
                 dest: 'build/unidos.min.js'
             }
+        },
+        shell:{
+            multiple: {
+                command: [
+                    'del unidos.js',
+                    'mkdir deploy',
+                    'move build\\unidos.min.js deploy\\unidos.min.js'
+                ].join('&&')
+            }
         }
+        
     });
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');   
+    grunt.loadNpmTasks('grunt-shell');   
     
     //default Task
-    grunt.registerTask('default',['jshint','concat','uglify']);
+    grunt.registerTask('default',['jshint','concat','uglify','shell']);
 };
